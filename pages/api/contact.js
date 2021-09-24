f7d4f6e6771a1e9d10868b8014e1260666f2a739
734n <734n@C02FC2MWMD6T.local>
2021-09-24 15:54:29 +0100

    added contact creds

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -24,13 +24,13 @@ export default async (req, res) => {
         port: 465,
         host: "smtp.gmail.com",
         auth: {
-            user: process.env.FORM_EMAIL,
-            pass: process.env.FORM_PASSWORD,
+            user: 'ariansamouiecoach@gmail.com',
+            pass: 'oqiwfsmxomnnzjpa',
         },
         secure: true,
     })
     const mailData = {
-        from: process.env.FORM_EMAIL,
+        from: 'ariansamouiecoach@gmail.com',
         to: 'ariansamouiecoach@gmail.com',
         subject: `Website - ${req.body.first_name} ${req.body.last_name}`,
         text: `<h1>${req.body.first_name} ${req.body.last_name}</h1> <a style="font-size: 20px" href="tel:${req.body.telephone}">${req.body.telephone}</a> <p>${req.body.email}</p> <p>${req.body.message}</p>`,
