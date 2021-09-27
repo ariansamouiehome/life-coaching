99b0f515542848eeeb3a82d09e25a328d9d152c2
734n <734n@ip-192-168-0-10.eu-west-1.compute.internal>
2021-09-27 13:37:24 +0100

    new

--- pages/api/getEmailListEntry.js
+++ pages/api/getEmailListEntry.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 const path = require("path");
 
 export default async (req, res) => {
-
+    //
     // const { readFileSync } = require('fs')
     // const { join } = require('path')
-    // const array = readFileSync(join(__dirname, '../../files', 'emailList.txt'), 'utf8').toString();
+    // const array = readFileSync(join(__dirname, '/files', 'emailList.txt'), 'utf8').toString();
 
 
     const array = fs.readFileSync('files/emailList.txt').toString();
