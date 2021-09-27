509e77e65d0bbb3e58d6bad14771b458dca1148a
734n <734n@ip-192-168-0-10.eu-west-1.compute.internal>
2021-09-27 13:41:46 +0100

    back to working

--- pages/api/getEmailListEntry.js
+++ pages/api/getEmailListEntry.js
@@ -1,13 +1,6 @@
-import serverPath from "/utils/resolveFiles";
 const fs = require('fs');
-const path = require("path");
 
 export default async (req, res) => {
-    //
-    // const { readFileSync } = require('fs')
-    // const { join } = require('path')
-    // const array = readFileSync(join(__dirname, '/files', 'emailList.txt'), 'utf8').toString();
-
 
     const array = fs.readFileSync('files/emailList.txt').toString();
 
