67fe1b9f9e2227c81363ab43d84f0b19832abcf7
734n <734n@ip-192-168-0-10.eu-west-1.compute.internal>
2021-09-27 13:34:35 +0100

    path older

--- pages/api/getEmailListEntry.js
+++ pages/api/getEmailListEntry.js
@@ -4,12 +4,12 @@ const path = require("path");
 
 export default async (req, res) => {
 
-    const { readFileSync } = require('fs')
-    const { join } = require('path')
-    const array = readFileSync(join(__dirname, '../../files', 'emailList.txt'), 'utf8').toString();
+    // const { readFileSync } = require('fs')
+    // const { join } = require('path')
+    // const array = readFileSync(join(__dirname, '../../files', 'emailList.txt'), 'utf8').toString();
 
 
-    // const array = fs.readFileSync('files/emailList.txt').toString();
+    const array = fs.readFileSync('files/emailList.txt').toString();
 
     try {
         return res.status(200).json({ data:JSON.parse(array) });
