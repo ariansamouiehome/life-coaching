59c4b107a01dafbecc312ab18a9b2cc83539dc42
734n <734n@C02FC2MWMD6T.local>
2022-01-02 16:04:39 +0000

    updated email list sending

--- components/AdminPanel/index.jsx
+++ components/AdminPanel/index.jsx
@@ -4,14 +4,13 @@ import FormInput from "components/Elements/FormInput";
 import Button from "components/Elements/Button";
 import axios from "axios";
 import Link from "next/link";
+import {globalMailingList} from "../../utils/mailingList";
 
 const AdminPanel = () => {
 
     // States
-    const [view, setView] = useState(1);
     const [showSureButton, setShowSureButton] = useState(false);
     const [success, setSuccess] = useState(false);
-    const [contactList, setContactList] = useState([]);
     const [form, setForm] = useState({
         password: ''
     });
@@ -22,8 +21,6 @@ const AdminPanel = () => {
         href: '',
     });
 
-    console.log(mailingList);
-
     // Data
     const password = 'qwerty';
 
@@ -31,25 +28,19 @@ const AdminPanel = () => {
     const handleSubmit = () => {
         if (form.password === password) {
             setSuccess(true);
-            getList();
         }
     }
 
     const sendMailingList = () => {
-        axios.post('/api/mailingList', mailingList)
-            .then(data => {
-                console.log(data);
-            })
-            .catch(err => {
-                // console.log(err);
-            })
-    };
-
-    const getList = () => {
-        axios.get('/api/getContactEntries')
-            .then(data => {
-                setContactList(data.data.data);
-            })
+        // for (let i = 0; i < globalMailingList.length; i++) {
+        //     axios.post('/api/mailingList', {email:globalMailingList[i]})
+        //         .then(data => {
+        //             console.log(data);
+        //         })
+        //         .catch(err => {
+        //             console.log(err);
+        //         })
+        // }
     };
 
     return (
@@ -71,79 +62,48 @@ const AdminPanel = () => {
                     </form>
                 </Col>}
                 {success && <>
-
-                <Col xs={12} className="admin-panel-nav">
-                    <Link href="/">
-                        <a className="admin-logo">
-                            <img src="/images/logo-single.svg" alt="Arian Samouie Life Coach" className="admin-logo-image"/>
-                        </a>
-                    </Link>
-                </Col>
-
-                <Col xs={12} className="admin-panel-nav nav">
-                    <Button onClick={() => (setView(1), getList())}>Website Contact</Button>
-                    <Button onClick={() => setView(2)}>Mailing list</Button>
-
-                </Col>
-
-                {view === 1 && <Col xs={12} className="admin-panel-login">
-                    <div className="admin-list">
-                        {contactList.length > 0 && contactList.reverse().map((item, key) => <div className="each-email-list">
-                            <div className="top-two">
-                                <h3 className="each-name">{item.first_name} {item.last_name}</h3>
-                                <span className="each-date">{item.date}</span>
-                            </div>
-                            <a href={`tel:${item.telephone}`} className="each-tel">{item.telephone}</a>
-                            <a href={`mailto:${item.email}`} className="each-email">{item.email}</a>
-                        </div>)}
-                    </div>
-                </Col>}
-
-                {view === 2 && <Col xs={12} className="admin-panel-login">
-                    <div className="mailing-list-form-wrapper">
-                        <FormInput
-                            form={mailingList}
-                            setForm={setMailingList}
-                            name="subject"
-                            value="subject"
-                            type="text"
-                            label="Subject"
-                            placeholder="Email subject"
-                        />
-                        <FormInput
-                            form={mailingList}
-                            setForm={setMailingList}
-                            name="title"
-                            value="title"
-                            type="text"
-                            label="Title"
-                            placeholder="Email template title"
-                        />
-                        <FormInput
-                            form={mailingList}
-                            setForm={setMailingList}
-                            name="bodyCopy"
-                            value="bodyCopy"
-                            type="textarea"
-                            label="Copy"
-                            placeholder="This has to be in html format in order to add (new lines - <p>)"
-                        />
-                        <FormInput
-                            form={mailingList}
-                            setForm={setMailingList}
-                            name="href"
-                            value="href"
-                            type="text"
-                            label="Link"
-                            placeholder="Type in the location the cta will take thee user"
-                        />
-
-                        <Button onClick={() => setShowSureButton(!showSureButton)}>
-                            {showSureButton ? 'go back' : 'Send Emails'}
-                        </Button>
-                        {showSureButton && <Button onClick={() => sendMailingList()} style={{marginTop: '30px'}}>confirm send emails?</Button>}
-                    </div>
-                </Col>}
+                    <Col xs={12} className="admin-panel-nav">
+                        <Link href="/">
+                            <a className="admin-logo">
+                                <img src="/images/logo-single.svg" alt="Arian Samouie Life Coach"
+                                     className="admin-logo-image"/>
+                            </a>
+                        </Link>
+                    </Col>
+                    <Col xs={12} className="admin-panel-login">
+                        <div className="mailing-list-form-wrapper">
+                            {/*<FormInput*/}
+                            {/*    form={mailingList}*/}
+                            {/*    setForm={setMailingList}*/}
+                            {/*    name="subject"*/}
+                            {/*    value="subject"*/}
+                            {/*    type="text"*/}
+                            {/*    label="Subject"*/}
+                            {/*    placeholder="Email subject"*/}
+                            {/*/>*/}
+                            {/*<FormInput*/}
+                            {/*    form={mailingList}*/}
+                            {/*    setForm={setMailingList}*/}
+                            {/*    name="bodyCopy"*/}
+                            {/*    value="bodyCopy"*/}
+                            {/*    type="textarea"*/}
+                            {/*    label="Copy"*/}
+                            {/*    placeholder="This has to be in html format in order to add (new lines - <p>)"*/}
+                            {/*/>*/}
+                            <Button
+                                onClick={() => setShowSureButton(!showSureButton)}
+                            >
+                                {showSureButton ? 'go back' : 'Send Emails'}
+                            </Button>
+                            {showSureButton &&
+                            <Button
+                                onClick={() => sendMailingList()} style={{marginTop: '15px'}}
+                                danger
+                            >
+                                confirm send emails?
+                            </Button>}
+                        </div>
+                    </Col>
                 </>}
             </Row>
         </Container>
--- pages/api/mailingList.js
+++ pages/api/mailingList.js
@@ -2,10 +2,9 @@ import React from "react";
 
 export default async (req, res) => {
 
-    const fs = require('fs');
-
-    const emailArray = fs.readFileSync('emailList.txt').toString();
-    const jsEmailArray = JSON.parse(emailArray);
+    // const fs = require('fs');
+    // const emailArray = fs.readFileSync('emailList.txt').toString();
+    // const jsEmailArray = JSON.parse(emailArray);
 
     let nodemailer = require('nodemailer')
     const transporter = nodemailer.createTransport({
@@ -19,44 +18,74 @@ export default async (req, res) => {
     })
     const mailData = {
         from: 'ariansamouiecoach@outlook.com',
-        to: 'ariansamouie@gmail.com',
-        subject: "Meditation Session",
+        to: req.body.email,
+        subject: 'Why We Seek External Validation? - New Youtube Video',
         text: `
                 <img src="https://ariansamouie.com/images/logo-single.svg" style="max-width: 400px;"/>
-                <h1>Arian Samouie Meditation Session</h1>
-                <p><strong>28th Dec 8pm (Zoom Call)</strong></p>
-                <h3>Hey Guys,</h3> 
-                <p>I hope you all had a great Christmas.</p>
-                <p>An exiting reminder that the meditation session is <u><strong>TODAY</strong></u>.</p>
-                <p>Just in case it helps, Here is the link for the session - <a href="https://us04web.zoom.us/j/76063040928?pwd=c2E2U21sVVNEdjNTWUlXZjNIY1hoUT09">CLICK HERE TO JOIN THE ZOOM CALL</a></p>
-                <h2><u>The Plan</u></h2> 
-                <p>The call will start at 8pm.</p> 
-                <p>The session will start soon after that.</p>
-                <p>Remember that all you will need is a pillow, Somewhere comfortable to lay down (maybe a bed), Zoom (the video calling app) and Preferably headphones.</p>
-                <p>I'm exited to see you there.</p>
+                <h1>Why we seek external validation?</h1>
+                
+                <p>We live in a society that has not caught up yet with how "the norms" are truly affecting us.</p>
+                <p>It can be very easy to get lost looking in the direction of what has been done to us and finding someone or something to blame for it.</p>
+                <p>To bring the reason to the light of our awareness is no doubt a step in our healing journey, but we need to be very aware of that we don't get stuck in this part of the journey.</p>
+                <br />
+                <p>There are many ways in how we have been programmed into seeking external validation, as adults and most humans that have lived on this planet have learnt that validation comes from the outside and not within.</p>
+                <p>One of the biggest reasons we have fallen into this category is to do with the way society brings up their children.</p>
+                <p>The good behaviour = reward, bad behaviour = punishment social control technique has many unwanted teachings that come with it.</p>
+                <p>There are multiple reason we feel like this, but this is one of the biggest reasons why we are programmed into seeking validation externally.</p>                
+                <br />
+                <p>Our healing is <strong>OUR</strong> responsibility.</p>
+                <p>No one has the ability to do it for us.</p>
+                <p>Just for being a living human, means that you have the ability to heal from anything that you have been through.</p>
+                <p>That strength is inside you, weather you choose to believe it or not.</p>
+                
+                <br />
+                <p>To learn more about external validation watch my YouTube video titled  <a href="https://www.youtube.com/watch?v=SaJq3Yx436M" target="_blank">"Why We Seek External Validation?"</a></p>
+                
+                <br />
+                <h2>To get involved with my <strong><u>FREE</u></strong> online meditation session <a href="https://ariansamouie.com/meditation-sessions" target="_blank">CLICK HERE</a></h2>
+                
+                <br />
+                <h2><strong>Life Coach</strong></h2>
+                <p>If you would like to work 121 with me then <a href="https://ariansamouie.com/book-a-call" target="_blank">Book a Call</a>, and I will get back to you.</p>
+                
                 <br />
                 <p>Kind regards,</p>
                 <p><strong>Arian Samouie</strong></p>
-                <br /> <h4><u>Social Media</u></h4> <p><a href="https://www.youtube.com/channel/UCJLf_QdB9icW3mhCPX66brg">Youtube</a> | <a href="https://www.instagram.com/arian_samouie/">Instagram</a> | <a href="https://www.facebook.com/arian.samouie/">Facebook</a> | <a href="https://www.tiktok.com/@arian_samouie?">TikTok</a> | <a href="https://soundcloud.com/arian_samouie">Soundcloud</a> | <a href="https://ariansamouie.com/">ariansamouie.com</a></p>
+                <br /> <h4><u>Social Media</u></h4> <p><a href="https://www.youtube.com/channel/UCJLf_QdB9icW3mhCPX66brg" style="color:#d2a94e;">Youtube</a> | <a href="https://www.instagram.com/arian_samouie/" style="color:#d2a94e;">Instagram</a> | <a href="https://www.facebook.com/arian.samouie/" style="color:#d2a94e;">Facebook</a> | <a href="https://www.tiktok.com/@arian_samouie?" style="color:#d2a94e;">TikTok</a> | <a href="https://soundcloud.com/arian_samouie" style="color:#d2a94e;">Soundcloud</a> | <a href="https://ariansamouie.com/" style="color:#d2a94e;">ariansamouie.com</a></p>
         `,
 
         html: `
                 <img src="https://ariansamouie.com/images/logo-single.svg" style="max-width: 400px;"/>
-                <h1>Arian Samouie Meditation Session</h1>
-                <p><strong>28th Dec 8pm (Zoom Call)</strong></p>
-                <h3>Hey Guys,</h3> 
-                <p>I hope you all had a great Christmas.</p>
-                <p>An exiting reminder that the meditation session is <u><strong>TODAY</strong></u>.</p>
-                <p>Just in case it helps, Here is the link for the session - <a href="https://us04web.zoom.us/j/76063040928?pwd=c2E2U21sVVNEdjNTWUlXZjNIY1hoUT09">CLICK HERE TO JOIN THE ZOOM CALL</a></p>
-                <h2><u>The Plan</u></h2> 
-                <p>The call will start at 8pm.</p> 
-                <p>The session will start soon after that.</p>
-                <p>Remember that all you will need is a pillow, Somewhere comfortable to lay down (maybe a bed), Zoom (the video calling app) and Preferably headphones.</p>
-                <p>I'm exited to see you there.</p>
+                <h1>Why we seek external validation?</h1>
+                
+                <p>We live in a society that has not caught up yet with how "the norms" are truly affecting us.</p>
+                <p>It can be very easy to get lost looking in the direction of what has been done to us and finding someone or something to blame for it.</p>
+                <p>To bring the reason to the light of our awareness is no doubt a step in our healing journey, but we need to be very aware of that we don't get stuck in this part of the journey.</p>
+                <br />
+                <p>There are many ways in how we have been programmed into seeking external validation, as adults and most humans that have lived on this planet have learnt that validation comes from the outside and not within.</p>
+                <p>One of the biggest reasons we have fallen into this category is to do with the way society brings up their children.</p>
+                <p>The good behaviour = reward, bad behaviour = punishment social control technique has many unwanted teachings that come with it.</p>
+                <p>There are multiple reason we feel like this, but this is one of the biggest reasons why we are programmed into seeking validation externally.</p>                
+                <br />
+                <p>Our healing is <strong>OUR</strong> responsibility.</p>
+                <p>No one has the ability to do it for us.</p>
+                <p>Just for being a living human, means that you have the ability to heal from anything that you have been through.</p>
+                <p>That strength is inside you, weather you choose to believe it or not.</p>
+                
+                <br />
+                <p>To learn more about external validation watch my YouTube video titled  <a href="https://www.youtube.com/watch?v=SaJq3Yx436M" target="_blank">"Why We Seek External Validation?"</a></p>
+                
+                <br />
+                <h2>To get involved with my <strong><u>FREE</u></strong> online meditation session <a href="https://ariansamouie.com/meditation-sessions" target="_blank">CLICK HERE</a></h2>
+                
+                <br />
+                <h2><strong>Life Coach</strong></h2>
+                <p>If you would like to work 121 with me then <a href="https://ariansamouie.com/book-a-call" target="_blank">Book a Call</a>, and I will get back to you.</p>
+                
                 <br />
                 <p>Kind regards,</p>
-                <p><strong>Arian Samouie</strong></p>  
-                <br /> <h4><u>Social Media</u></h4> <p><a href="https://www.youtube.com/channel/UCJLf_QdB9icW3mhCPX66brg">Youtube</a> | <a href="https://www.instagram.com/arian_samouie/">Instagram</a> | <a href="https://www.facebook.com/arian.samouie/">Facebook</a> | <a href="https://www.tiktok.com/@arian_samouie?">TikTok</a> | <a href="https://soundcloud.com/arian_samouie">Soundcloud</a> | <a href="https://ariansamouie.com/">ariansamouie.com</a></p>
+                <p><strong>Arian Samouie</strong></p>
+                <br /> <h4><u>Social Media</u></h4> <p><a href="https://www.youtube.com/channel/UCJLf_QdB9icW3mhCPX66brg" style="color:#d2a94e;">Youtube</a> | <a href="https://www.instagram.com/arian_samouie/" style="color:#d2a94e;">Instagram</a> | <a href="https://www.facebook.com/arian.samouie/" style="color:#d2a94e;">Facebook</a> | <a href="https://www.tiktok.com/@arian_samouie?" style="color:#d2a94e;">TikTok</a> | <a href="https://soundcloud.com/arian_samouie" style="color:#d2a94e;">Soundcloud</a> | <a href="https://ariansamouie.com/" style="color:#d2a94e;">ariansamouie.com</a></p>
         `,
     }
 
