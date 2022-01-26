fee3603b7825c538c09914edb35efb2987623db9
734n <734n@ip-192-168-0-10.eu-west-1.compute.internal>
2022-01-26 14:19:20 +0000

    started mailing list

---
+++ components/MailingListForm/index.jsx
@@ -0,0 +1,132 @@
+import React, {useEffect, useState} from 'react';
+import {Col, Container, Row} from "reactstrap";
+import axios from "axios";
+import FormInput from "components/Elements/FormInput";
+import Button from "components/Elements/Button";
+import AlertMessage from "components/AlertMessage";
+import {scrollToPosition} from "../../utils/functions";
+
+const MailingListForm = (props) => {
+
+    const {sessionDate} = props;
+
+    // Data
+    const contactForm = [
+        {
+            name: 'mailingListName',
+            type: 'text',
+            label: 'Name',
+            placeholder: 'Adam',
+            errorMessage: 'stop it'
+        },
+        {
+            name: 'mailingListEmail',
+            type: 'email',
+            label: 'Email',
+            placeholder: 'adam.smith@gmail.com'
+        }
+    ];
+    const currentDate = new Date().toLocaleDateString('en-GB');
+
+    // State
+    const [checkError, setCheckError] = useState(false);
+    const [errorActive, setErrorActive] = useState(false);
+    const [failedToSend, setFailedToSend] = useState(true);
+    const [success, setSuccess] = useState(false);
+    const [showMessage, setShowMessage] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [form, setForm] = useState({
+        name: '',
+        email: '',
+    });
+
+    // Function
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setShowMessage(false)
+        setFailedToSend(false);
+        setLoading(false);
+        setErrorActive(false);
+        setSuccess(false);
+
+        if (checkError) {
+            setLoading(true);
+            axios.post('/api/AAAAAA', form)
+                .then(data => {
+                    setForm({
+                        first_name: '',
+                        email: '',
+                    })
+                    setShowMessage(true);
+                    setFailedToSend(false);
+                    setSuccess(true);
+                    setLoading(false);
+                    scrollToPosition('contact-form-message');
+                    // console.log(data);
+                })
+                .catch(err => {
+                    setShowMessage(true);
+                    setLoading(false);
+                    setSuccess(false);
+                    setFailedToSend(true);
+                    scrollToPosition('contact-form-message');
+                    // console.log(err);
+                })
+        } else {
+            setErrorActive(true);
+            scrollToPosition('contact-form-form', 90);
+        }
+    }
+
+    useEffect(() => {
+        if (form.name.length === 0 ||
+            form.email.length === 0) {
+            setCheckError(false);
+        } else {
+            setCheckError(true);
+        }
+    }, [form]);
+
+    return (
+        <form onSubmit={(e) => handleSubmit(e)} className="contact-form-form mailing-list" id="contact-form-form">
+            <Container className="contact-form" data-aos>
+                <Row>
+                    <Col xs={12} className="mailing-list-header">
+                        <h3 className="mailing-list-heading">Mailing List</h3>
+                    </Col>
+                    {contactForm.map((item, key) => <Col xs={12} md={5} className="contact-form-each-input" key={`input-key-${key}`}>
+                        <FormInput
+                            form={form}
+                            setForm={setForm}
+                            name={item.name}
+                            value={item.name}
+                            type={item.type}
+                            label={item.label}
+                            placeholder={item.placeholder}
+                            errorActive={errorActive}
+                            loading={loading}
+                        />
+                    </Col>)}
+                    <Col xs={12} md={2} className="contact-form-footer">
+                        <Button
+                            type="submit"
+                            loading={loading}
+                            disabled={checkError}
+                        >
+                            Submit
+                        </Button>
+                    </Col>
+                    <Col xs={12}  className="contact-form-message" id="contact-form-message">
+                        {showMessage && <AlertMessage
+                            success={success}
+                            successMessage="Your position has been reserved. An email with a zoom link and information about the session has been sent to you."
+                            failed={failedToSend}
+                        />}
+                    </Col>
+                </Row>
+            </Container>
+        </form>
+    )
+}
+
+export default MailingListForm;
