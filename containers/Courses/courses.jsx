dd45e6095ddf7e2d71a5086d3d96279cb1e06893
734n <734n@ip-192-168-0-10.eu-west-1.compute.internal>
2022-02-14 12:13:41 +0000

    started

--- containers/Courses/courses.jsx
+++ containers/Courses/courses.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import FullWidthImageTextBanner from "components/FullWidthImageTextBanner";
 import AppHead from "components/Layout/AppHead";
 import PageNotFound from "components/PageNotFound";
+import ColumnInfoImage from "components/ColumnInfoImage";
+import Clickable from "components/Elements/Clickable";
+import BackImageFloatingContent from "components/BackImageFloatingContent";
 
 const Courses = (props) => {
 
     if (props.data) {
-        const {title, description, backgroundImage} = props.data;
+        const {title, description, backgroundImage, card_description} = props.data;
         return (
             <>
                 <AppHead
@@ -16,11 +19,29 @@ const Courses = (props) => {
                 />
                 <FullWidthImageTextBanner
                     title={title}
-                    description={description}
+                    description={card_description}
                     backgroundImage={backgroundImage}
                     contentCenter
                     alt
+                    // href="/courses"
+                    // cta_text="Back to courses"
                 />
+
+                <ColumnInfoImage
+                    title="What is the goal?"
+                    description={description}
+                    headingLine
+                    paddingBottom
+                />
+
+                <BackImageFloatingContent
+                    backgroundImage="/images/pattern-back-4.png"
+                    title="What is in this course?"
+                    headingLine
+                    curveBottom
+                >
+                    <p className="body-copy">W</p>
+                </BackImageFloatingContent>
             </>
         )
     } else {
