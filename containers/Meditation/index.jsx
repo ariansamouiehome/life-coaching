725ceb18a409ee9d0a2c61ac39df2fc740009887
734n <734n@ip-192-168-0-10.eu-west-1.compute.internal>
2022-02-22 21:12:38 +0000

    changed date med

--- containers/Meditation/index.jsx
+++ containers/Meditation/index.jsx
@@ -11,7 +11,7 @@ import TestimonialForm from "components/TestimonialForm";
 const Meditation = () => {
 
     // Data
-    const sessionDate = '22nd Feb - 8:00pm';
+    const sessionDate = '8th March - 8:00pm';
     const meditation = {
         date: sessionDate,
         name: 'How To Let Go - Becoming The New You',
--- pages/api/mailingList.js
+++ pages/api/mailingList.js
@@ -16,29 +16,28 @@ export default async (req, res) => {
         },
         secure: true,
     })
-    const subject = 'New Meditation Sessions';
+    const subject = 'Meditation Sessions Tonight';
     const bodyText = `
         <img src="https://ariansamouie.com/images/logo-single.svg" style="max-width: 320px;"/>
-        <h1>What do they do?</h1>
-        
+        <h1>How To Let Go - Becoming The New You</h1>
+        <p>Tuesday 22nd 8pm (TODAY)</p>
+        <br />
         <h2>Hi ${req.body.firstName}.</h2>   
+        <p>A friendly reminder about the meditation session tonight.</p>
+        <p>I'm looking forward to seeing you there.</p>
         
-        <p>I'm hosting 3 meditation sessions.</p>
         <br/>
-        <p>I've moved to MeetUp and will be using this platform for all my onlne meditation sessions.</p>
-        <p>In this video I explain what these sessions are for and how each meditation helps you heal yourself.</p>
-
-        <a href="https://www.youtube.com/watch?v=dB4-nvmaPoY" target="_blank"
-            style="width: 100%; max-width: 320px; display: block; padding: 15px; text-align: center; color: #fff; background: #c4302b; text-decoration: none; border-radius: 5px;"
+        <p>Just in case it helps, here's a link for the session tonight.</p>
+        <a href="https://us04web.zoom.us/j/76063040928?pwd=c2E2U21sVVNEdjNTWUlXZjNIY1hoUT09" target="_blank"
+            style="width: 100%; max-width: 320px; display: block; padding: 15px; text-align: center; color: #fff; background: #2D8CFF; text-decoration: none; border-radius: 5px;"
         >
-            Watch Video
-        </a> 
+            Join Zoom Call
+        </a>
+        
         <br />
         <br />
         
-        <p>To stay up to date with everything that is going on and be notified when I create new sessions, join my MeetUp group.</p>
-        <br />
-        <p><strong>To join my MeetUp group about all meditation sessions I will be hosting, click on the button below.</strong></p>
+        <p>To stay up to date with all the news about my new meditation sessions, join my MeetUp group.</p>
         <a href="https://www.meetup.com/online-healing-meditation/" target="_blank"
             style="width: 100%; max-width: 320px; display: block; padding: 15px; text-align: center; color: #fff; background: #e51937; text-decoration: none; border-radius: 5px;"
         >
@@ -46,19 +45,19 @@ export default async (req, res) => {
         </a>
         
         
-        <br />
-        <p><strong>Alternativley you can join by reserving your place on my website, click the button below.</strong></p>
-        <a href="https://ariansamouie.com/meditation-sessions" target="_blank"
-            style="width: 100%; max-width: 320px; display: block; padding: 15px; text-align: center; color: #fff; background: #d2a94e; text-decoration: none; border-radius: 5px;"
-        >
-            Reserve Your Position
-        </a>
+<!--        <br />-->
+<!--        <p><strong>Alternativley you can join by reserving your place on my website, click the button below.</strong></p>-->
+<!--        <a href="https://ariansamouie.com/meditation-sessions" target="_blank"-->
+<!--            style="width: 100%; max-width: 320px; display: block; padding: 15px; text-align: center; color: #fff; background: #d2a94e; text-decoration: none; border-radius: 5px;"-->
+<!--        >-->
+<!--            Reserve Your Position-->
+<!--        </a>-->
         
         <br />
         <p>To learn more about why we do what we do, visit my <a href="https://www.youtube.com/channel/UCJLf_QdB9icW3mhCPX66brg" style="color:#d2a94e;">YouTube channel</a> to watch my videos.</p>
         
         
-        <br /> <h4><u>Social Media</u></h4> <p><a href="https://www.youtube.com/channel/UCJLf_QdB9icW3mhCPX66brg" style="color:#d2a94e;">Youtube</a> | <a href="https://www.instagram.com/arian_samouie/" style="color:#d2a94e;">Instagram</a> | <a href="https://www.facebook.com/arian.samouie/" style="color:#d2a94e;">Facebook</a> | <a href="https://www.tiktok.com/@arian_samouie?" style="color:#d2a94e;">TikTok</a> | <a href="https://soundcloud.com/arian_samouie" style="color:#d2a94e;">Soundcloud</a> | <a href="https://ariansamouie.com/" style="color:#d2a94e;">ariansamouie.com</a></p>
+        <br /><br /> <h4><u>Social Media</u></h4> <p><a href="https://www.youtube.com/channel/UCJLf_QdB9icW3mhCPX66brg" style="color:#d2a94e;">Youtube</a> | <a href="https://www.instagram.com/arian_samouie/" style="color:#d2a94e;">Instagram</a> | <a href="https://www.facebook.com/arian.samouie/" style="color:#d2a94e;">Facebook</a> | <a href="https://www.tiktok.com/@arian_samouie?" style="color:#d2a94e;">TikTok</a> | <a href="https://soundcloud.com/arian_samouie" style="color:#d2a94e;">Soundcloud</a> | <a href="https://ariansamouie.com/" style="color:#d2a94e;">ariansamouie.com</a></p>
         
         <br />
         <p>Kind regards,</p>
