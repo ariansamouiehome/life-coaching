5123d7d40131c1afae3fbb92258ef60b56fb4ba7
734n <ariansamouie@yahoo.co.uk>
2022-07-29 13:33:25 +0100

    added link thumbnail

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react";
 import 'styles/app.scss';
 import Router, {useRouter} from 'next/router';
 import {PageChangeContext} from "../utils/pageChangeContext";
+import AppHead from "components/Layout/AppHead";
 
 const MyApp = ({Component, pageProps}) => {
     // Data
@@ -13,13 +14,18 @@ const MyApp = ({Component, pageProps}) => {
     //     },100)
     // },[])
     return (
-        <PageChangeContext.Provider >
+        <PageChangeContext.Provider>
             {/*{show && <Header*/}
             {/*    showNav={showNav}*/}
             {/*    setShowNav={setShowNav}*/}
             {/*/>}*/}
-            <Component {...pageProps} />
+            {/*<Component {...pageProps} />*/}
             {/*<Footer show={show}/>*/}
+            <AppHead
+                title="Mind Matter Events"
+                description="Mind Matters Events is a place to expand your knowledge and learn how to heal your emotional wounds. A place that gives you the understanding and awareness required to progress on your healing path in order to connect deeper to your natural state and become your most authentic healed self."
+                image="/images/mind-matters.jpeg"
+            />
         </PageChangeContext.Provider>
     )
 }
